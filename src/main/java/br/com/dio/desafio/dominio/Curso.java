f308c8fa8aacf6ae630209745350fb04f3160ca5
bruno.almd <brunoalmeida22007@gmail.com>
2026-01-19 15:17:08 -0300

    Mentoria e cursos

---
+++ src/main/java/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,49 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+    private String titulo;
+    private String descricao;
+    private double cargaHoraria;
+
+    public Curso() {
+    }
+
+    public double calcularXp()
+    {
+        return 0.0;
+    }
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(double cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "cargaHoraria=" + cargaHoraria +
+                ", titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+}
