9fee403fbf96e5c4df9eb2b55dd46d88fea65829
bruno.almd <brunoalmeida22007@gmail.com>
2026-01-19 15:15:46 -0300

    Mentoria e Curso

---
+++ src/main/java/br/com/dio/desafio/Main.java
@@ -0,0 +1,29 @@
+package br.com.dio.desafio;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descricao para o curso java");
+        curso1.setCargaHoraria(8);
+        System.out.println(curso1);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso javaScript");
+        curso2.setDescricao("descricao para o curso javaScript");
+        curso2.setCargaHoraria(4);
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descricao mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+    }
+}
